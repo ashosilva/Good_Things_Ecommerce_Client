56027d39eecb8bb811966758a610169a4886d375
Abayomi Shosilva <ashosilva@gmail.com>
2023-06-15 08:38:47 -0400

    tweaking for blank page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { Fragment,useState, useEffect } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import Pagination from 'react-js-pagination'
 
 import Slider from 'rc-slider'
@@ -17,7 +17,7 @@ const { createSliderWithTooltip } = Slider
 const Range = createSliderWithTooltip(Slider.Range)
 
 
-const Home = ({match}) => {
+const Home = ({ match }) => {
 
     const [currentPage, setCurrentPage] = useState(1)
     const [price, setPrice] = useState([1, 1000])
@@ -38,16 +38,16 @@ const Home = ({match}) => {
         'Health',
         'Sports',
         'Outdoor',
-        'Home' 
+        'Home'
     ]
 
     const alert = useAlert()
     const dispatch = useDispatch()
 
-    const { loading, products, error, productsCount, resPerPage, filteredProductsCount} = useSelector(state => state.products)
+    const { loading, products, error, productsCount, resPerPage, filteredProductsCount } = useSelector(state => state.products)
 
     const keyword = match.params.keyword
-    
+
 
     useEffect(() => {
         if (error) {
@@ -70,7 +70,7 @@ const Home = ({match}) => {
     }
 
     return (
-        
+
         <Fragment>
             {loading ? <Loader /> :
                 <Fragment>
@@ -79,113 +79,104 @@ const Home = ({match}) => {
 
                     <section id="products" className="container mt-5">
                         <div className="row">
+                            {keyword ? (
+                                <Fragment>
+                                    <div className="col-6 col-md-3 mt-5 mb-5">
+                                        <div className="px-5">
+                                            <Range
+                                                marks={{
+                                                    1: `$1`,
+                                                    1000: `$1000`
 
-                            <div className="col-6 col-md-9">
-                                <div className="row">
-                                    {products && products.map(product => (
-                                        <Product key={product._id} product={product} col={4} />
-                                    ))}
-                                </div>
-                            </div>
+                                                }}
+                                                min={1}
+                                                max={1000}
+                                                defaultValue={[1, 1000]}
+                                                tipFormatter={value => `$${value}`}
+                                                tipProps={{
+                                                    placement: "top",
+                                                    visible: true
+                                                }}
+                                                value={price}
+                                                onChange={price => setPrice(price)}
+                                            />
 
-                            {/* {keyword ? (
-                                    <Fragment>
-                                        <div className="col-6 col-md-3 mt-5 mb-5">
-                                            <div className="px-5">
-                                                <Range
-                                                    marks={{
-                                                        1 : `$1`,
-                                                        1000 : `$1000`
-                                                        
-                                                    }}
-                                                    min={1}
-                                                    max={1000}
-                                                    defaultValue={[1, 1000]}
-                                                    tipFormatter={value => `$${value}`}
-                                                    tipProps={{
-                                                        placement: "top",
-                                                        visible: true
-                                                    }}
-                                                    value={price}
-                                                    onChange={price => setPrice(price)}
-                                                />
+                                            <hr className="my-5" />
+                                            <div className="mt-5">
+                                                <h4 className="mb-3">
+                                                    Categories
+                                                </h4>
 
-                                                <hr className="my-5" />
-                                                <div className="mt-5">
-                                                    <h4 className="mb-3">
-                                                        Categories
-                                                    </h4>
-                                                    
-                                                    <ul className="pl-0">
-                                                        {categories.map(category => (
-                                                            <li
-                                                                className="hover:text-yellow-400"
-                                                                style={{
-                                                                    cursor:'pointer',
-                                                                    listStyleType:'none'
-                                                                }}
-                                                                key={category}
-                                                                onClick={() => setCategory(category)
-                                                                }
-                                                            >
-                                                                {category}
-                                                            </li>
-                                                        ))}
-                                                    </ul>
+                                                <ul className="pl-0">
+                                                    {categories.map(category => (
+                                                        <li
+                                                            className="hover:text-yellow-400"
+                                                            style={{
+                                                                cursor: 'pointer',
+                                                                listStyleType: 'none'
+                                                            }}
+                                                            key={category}
+                                                            onClick={() => setCategory(category)
+                                                            }
+                                                        >
+                                                            {category}
+                                                        </li>
+                                                    ))}
+                                                </ul>
 
-                                                </div>
+                                            </div>
 
-                                                <hr className="my-3" />
-                                                <div className="mt-5">
-                                                    <h4 className="mb-3">
-                                                        Ratings
-                                                    </h4>
-                                                    
-                                                    <ul className="pl-0">
-                                                        {[5, 4, 3, 2, 1].map(star => (
-                                                            <li
-                                                                className="rating-hover"
-                                                                style={{
-                                                                    cursor:'pointer',
-                                                                    listStyleType:'none'
-                                                                }}
-                                                                key={star}
-                                                                onClick={() => setRating(star)
-                                                                }
-                                                            >
-                                                                <div className="rating-outer">
-                                                                    <div className="rating-inner"
-                                                                        style={{
-                                                                            width: `${star * 20}%`
-                                                                        }}
-                                                                    >
+                                            <hr className="my-3" />
+                                            <div className="mt-5">
+                                                <h4 className="mb-3">
+                                                    Ratings
+                                                </h4>
 
-                                                                    </div>
+                                                <ul className="pl-0">
+                                                    {[5, 4, 3, 2, 1].map(star => (
+                                                        <li
+                                                            className="rating-hover"
+                                                            style={{
+                                                                cursor: 'pointer',
+                                                                listStyleType: 'none'
+                                                            }}
+                                                            key={star}
+                                                            onClick={() => setRating(star)
+                                                            }
+                                                        >
+                                                            <div className="rating-outer">
+                                                                <div className="rating-inner"
+                                                                    style={{
+                                                                        width: `${star * 20}%`
+                                                                    }}
+                                                                >
+
                                                                 </div>
-                                                            </li>
-                                                        ))}
-                                                    </ul>
-
-                                                </div>
+                                                            </div>
+                                                        </li>
+                                                    ))}
+                                                </ul>
 
                                             </div>
-                                        </div>
-                                        
-                                        <div className="col-6 col-md-9">
-                                            <div className="row">
-                                                {products && products.map(product => (
-                                            <Product key={product._id} product={product} col={4}/>
-                                                ))}
-                                            </div>
-                                        </div>
-                                    </Fragment>
 
-                                ): (
-                                    products.map(product => (
-                                        <Product key={product._id} product={product} col={3}/>
-                                    )) 
-                                )} */}
-                            
+                                        </div>
+                                    </div>
+
+                                    <div className="col-6 col-md-9">
+                                        <div className="row">
+                                            {products && products.map(product => (
+                                                <Product key={product._id} product={product} col={4} />
+                                            ))}
+                                        </div>
+                                    </div>
+                                </Fragment>
+
+                            ) : (
+                                products.map(product => (
+                                    <Product key={product._id} product={product} col={3} />
+                                ))
+                            )}
+
 
                         </div>
                     </section>
@@ -207,7 +198,7 @@ const Home = ({match}) => {
 
                         </div>
                     )}
-                    
+
 
                 </Fragment>}
         </Fragment>
